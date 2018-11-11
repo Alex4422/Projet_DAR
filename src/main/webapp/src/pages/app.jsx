72e506b2fbe44cb2ba14e028c32546c0eee027b3
Christopher <trublereau@msn.com>
2018-11-11 16:29:15 +0100

    /show_page_episodes_seasons: Auto stash before revert of "Blabla"

--- src/main/webapp/src/pages/app.jsx
+++ src/main/webapp/src/pages/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {Route, NavLink, HashRouter} from "react-router-dom";
 
+
+
+
 import HomePage from './home';
 import RoutedMenuBar from '../components/menuBar/menuBar';
 import SearchPage from "./search";
