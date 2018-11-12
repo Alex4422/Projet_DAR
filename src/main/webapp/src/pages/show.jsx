0bf82cbdc8087949af919e41044d01b64da82323
Christopher <trublereau@msn.com>
2018-11-13 00:51:12 +0100

    Enrichment of the list of seasons and episodes

--- src/main/webapp/src/pages/show.jsx
+++ src/main/webapp/src/pages/show.jsx
@@ -8,6 +8,8 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
 import Tv from '@material-ui/icons/Tv';
+import LiveTv from '@material-ui/icons/LiveTv';
+import InfoOutlined from '@material-ui/icons/InfoOutlined';
 import Collapse from '@material-ui/core/Collapse';
 import Checkbox from '@material-ui/core/Checkbox';
 import {withRouter} from 'react-router-dom'
@@ -52,81 +54,93 @@ class ShowDetailsPage extends React.Component {
                             {this.state.overview}
                         </Typography>
                     </div>
-                </div>
-                <div style={this.listStyle()}>
-                    <List component="nav"
-                          subheader={<ListSubheader component="div">Seasons</ListSubheader>}
-                    >
-                        <ListItem button onClick={() => {
-                            this.setState(state => ({ openSeasons: !this.state.openSeasons }))}
-                        }>
-                            <ListItemIcon>
-                                <Tv />
-                            </ListItemIcon>
-                            <ListItemText inset primary="Seasons" />
-                            {this.state.openSeasons ? <ExpandLess /> : <ExpandMore />}
-                         </ListItem>
-                        <div style={this.nestedSeasonStyle()}>
-                            <Collapse in={this.state.openSeasons} timeout="auto" unmountOnExit>
-                                <List component="div" disablePadding
-                                      subheader={<ListSubheader component="div">Season 1</ListSubheader>}>
-                                    <ListItem button onClick={() => {
-                                        this.setState(state => ({ openSeason: !this.state.openSeason }))}
-                                    }>
-                                        <ListItemIcon>
-                                            <Tv />
-                                        </ListItemIcon>
-                                        <ListItemText inset primary="Season 1" />
-                                        {this.state.openSeason ? <ExpandLess /> : <ExpandMore />}
-                                    </ListItem>
-                                    <div style={this.nestedEpsStyle()}>
-                                        <Collapse in={this.state.openSeason} timeout="auto" unmountOnExit>
-                                            <List component="div" disablePadding
-                                                  subheader={<ListSubheader component="div">Episodes</ListSubheader>}>
-                                                <ListItem button onClick={() => {
-                                                    this.setState(state => ({ openEps: !this.state.openEps }))}
-                                                }>
-                                                    <ListItemIcon>
-                                                        <Tv />
-                                                    </ListItemIcon>
-                                                    <ListItemText inset primary="Episodes" />
-                                                    {this.state.openEps ? <ExpandLess /> : <ExpandMore />}
-                                                </ListItem>
-                                                <div style={this.nestedEpStyle()}>
-                                                    <Collapse in={this.state.openEps} timeout="auto" unmountOnExit>
-                                                        <List component="div" disablePadding
-                                                              subheader={<ListSubheader component="div">Episode 1</ListSubheader>}>
-                                                            <ListItem button onClick={() => {
-                                                                this.setState(state => ({ openEp: !this.state.openEp }))}
-                                                            }>
-                                                                <ListItemIcon>
-                                                                    <Tv />
-                                                                </ListItemIcon>
-                                                                <ListItemText inset primary="Episode 1" />
-                                                                {this.state.openEp ? <ExpandLess /> : <ExpandMore />}
-                                                            </ListItem>
-                                                            <div style={this.nestedEpDescrStyle()}>
-                                                                <Collapse in={this.state.openEp} timeout="auto" unmountOnExit>
-                                                                    <List component="div" disablePadding>
-                                                                        <ListItem button>
-                                                                            <ListItemIcon>
-                                                                                <Tv />
-                                                                            </ListItemIcon>
-                                                                            <ListItemText inset primary="Description" />
-                                                                        </ListItem>
-                                                                    </List>
-                                                                </Collapse>
-                                                            </div>
-                                                        </List>
-                                                    </Collapse>
-                                                </div>
-                                            </List>
-                                        </Collapse>
-                                    </div>
-                                </List>
-                            </Collapse>
-                        </div>
-                    </List>
+
+                    <div style={this.listStyle()}>
+                        <List component="nav">
+                            <ListItem button onClick={() => {
+                                this.setState(state => ({ openSeasons: !this.state.openSeasons }))}
+                            }>
+                                <ListItemIcon style={{color: '#000'}}>
+                                    <Tv />
+                                </ListItemIcon>
+                                <ListItemText inset primary={
+                                    <Typography style={this.listTitleSeasonStyle()}>Seasons</Typography>
+                                }/>
+                                {this.state.openSeasons ? <ExpandLess /> : <ExpandMore />}
+                            </ListItem>
+
+                            <div style={this.nestedSeasonStyle()}>
+                                <Collapse in={this.state.openSeasons} timeout="auto" unmountOnExit>
+                                    <List component="div" disablePadding>
+                                        <ListItem button onClick={() => {
+                                            this.setState(state => ({ openSeason: !this.state.openSeason }))}
+                                        }>
+                                            <ListItemIcon style={{color: '#000'}}>
+                                                <Tv />
+                                            </ListItemIcon>
+                                            <ListItemText inset primary={
+                                                <Typography style={this.listTitleSeasonStyle()}>Season 1</Typography>
+                                            }/>
+                                            {this.state.openSeason ? <ExpandLess /> : <ExpandMore />}
+                                        </ListItem>
+
+                                        <div style={this.nestedEpsStyle()}>
+                                            <Collapse in={this.state.openSeason} timeout="auto" unmountOnExit>
+                                                <List component="div" disablePadding>
+                                                    <ListItem button onClick={() => {
+                                                        this.setState(state => ({ openEps: !this.state.openEps }))}
+                                                    }>
+                                                        <ListItemIcon style={{color: '#000'}}>
+                                                            <LiveTv />
+                                                        </ListItemIcon>
+                                                        <ListItemText inset primary={
+                                                            <Typography style={this.listTitleEpStyle()}>Episodes</Typography>
+                                                        }/>
+                                                        {this.state.openEps ? <ExpandLess /> : <ExpandMore />}
+                                                    </ListItem>
+
+                                                    <div style={this.nestedEpStyle()}>
+                                                        <Collapse in={this.state.openEps} timeout="auto" unmountOnExit>
+                                                            <List component="div" disablePadding>
+                                                                <ListItem button onClick={() => {
+                                                                    this.setState(state => ({ openEp: !this.state.openEp }))}
+                                                                }>
+                                                                    <ListItemIcon style={{color: '#000'}}>
+                                                                        <LiveTv />
+                                                                    </ListItemIcon>
+                                                                    <ListItemText inset primary={
+                                                                        <Typography style={this.listTitleEpStyle()}>Episode 1</Typography>
+                                                                    }/>
+                                                                    {this.state.openEp ? <ExpandLess /> : <ExpandMore />}
+                                                                </ListItem>
+
+                                                                <div style={this.nestedEpDescrStyle()}>
+                                                                    <Collapse in={this.state.openEp} timeout="auto" unmountOnExit>
+                                                                        <List component="div" disablePadding>
+                                                                            <ListItem button>
+                                                                                <ListItemIcon style={{color: '#000'}}>
+                                                                                    <InfoOutlined />
+                                                                                </ListItemIcon>
+                                                                                <ListItemText inset primary={
+                                                                                    <Typography style={this.listTitleDescrStyle()}>Description</Typography>
+                                                                                }/>
+                                                                            </ListItem>
+
+                                                                        </List>
+                                                                    </Collapse>
+                                                                </div>
+                                                            </List>
+                                                        </Collapse>
+                                                    </div>
+                                                </List>
+                                            </Collapse>
+                                        </div>
+                                    </List>
+                                </Collapse>
+                            </div>
+                        </List>
+                    </div>
+
                 </div>
             </div>
         );
@@ -146,8 +160,35 @@ class ShowDetailsPage extends React.Component {
     listStyle() {
         return {
             display: 'flex',
-            flexDirection: 'column',
-            color: '#000000',
+            flexDirection: 'row',
+        }
+    }
+
+    listTitleSeasonStyle() {
+        return {
+            color: '#000',
+            fontSize: '150%',
+        }
+    }
+
+    listTitleEpStyle() {
+        return {
+            color: '#000',
+            fontSize: '120%',
+        }
+    }
+
+    listTitleDescrStyle() {
+        return {
+            color: '#000',
+            fontSize: '100%',
+        }
+    }
+
+    subheadStyle() {
+        return {
+            color: '#000',
+            fontSize: '150%'
         }
     }
 
